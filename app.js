7bc5daa851b8c153fedebe0cf91b124efa3dd9dc
miami78 <miamilarry78@gmail.com>
2019-09-27 18:13:18 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+exports.generateText = (name, age) => {
+    // Returns output text
+    return `${name} (${age})`;
+  };
---
+++ app.test.js
@@ -0,0 +1,6 @@
+const { generateText } = require('./app');
+
+test('should output name and age', () => {
+    const text = generateText('Larry', 23);
+    expect(text).toBe('Larry (23)');
+});
